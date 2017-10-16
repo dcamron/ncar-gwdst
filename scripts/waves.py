e60eea30d608635a141b311b12365ef1f75729c3
dcamron <mew2dude@gmail.com>
2017-10-16 16:10:49 -0600

    Updated Waves notebook, working on plotting lognormal over histogram. Restructure dataset?

--- scripts/waves.py
+++ scripts/waves.py
@@ -13,7 +13,7 @@ import seaborn as sns
 sns.set(context='talk', palette='colorblind', style='ticks')
 
 #####
-
+'''
 ds = xr.open_mfdataset(sorted(glob('/glade/scratch/mcamron/archive/f.c54120.FWscHIST.f09_f09.gwdst/atm/hist/f.c54120.FWscHIST.f09_f09.gwdst.cam.h1.2002-0[6,7]*')))
 
 ds = ds.sel(lat=slice(-20,20))
@@ -25,6 +25,7 @@ gwy = xr.Dataset({var: ds[var] for var in ds.data_vars if 'BTAUY' in var}) * 100
 tau = xr.Dataset({str(i): xu.sqrt(xu.square(gwx[x]) + xu.square(gwy[y])) for (i, (x,y)) in enumerate(zip(gwx.data_vars, gwy.data_vars))})
 
 tau.to_netcdf('/glade/scratch/mcamron/output/gwdst/tau_magnitude.nc')
+'''
 
 tau = xr.open_dataset('/glade/scratch/mcamron/output/gwdst/tau_magnitude.nc', chunks={'time': 61})
 
@@ -34,6 +35,9 @@ hist = 0
 for (i, x) in enumerate(tau.data_vars):
     (h, edges) = np.histogram(tau[str(i)], bins=bins)
     hist += h
+
+tau_hist = xr.DataArray(hist, coords={'edges': edges}, dims='bins')
+tau_hist.to_netcdf('/glade/scratch/mcamron/output/gwdst/tau_hist.nc')
 
 f, ax = plt.subplots(figsize=(7, 6))
 ax.step(edges[:-1], hist, where='post', linewidth=2)
@@ -45,4 +49,4 @@ ax.set_xlabel('Flux [mPa]')
 
 plt.tight_layout()
 
-f.savefig('../figures/waves/dist_flux.png')
+f.savefig('~/python/gwdst/figures/waves/dist_flux.png')
